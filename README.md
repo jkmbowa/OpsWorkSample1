a34ca72efca162dc1d24ae67ce21c390718a564c
James Mbowa <james.mbowa@bjss.com>
2018-07-10 22:59:19 +0100

    Sample1 cookbook

---
+++ README.md
@@ -0,0 +1,4 @@
+# Sample1
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,6 @@
+#
+# Cookbook:: Sample1
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
+Chef::Log.info("************* Hello World!!! *************")
