2ccb3661df3efc8f754b2f611485b736887e8714
James Mbowa <james.mbowa@bjss.com>
2018-07-12 14:04:36 +0100

    corrected path

--- recipes/InstallApache.rb
+++ recipes/InstallApache.rb
@@ -2,7 +2,7 @@ package "install Apache" do
     package_name "httpd"
 end
 
-file '/var/www/index.html' do
+file '/var/www/html/index.html' do
     content '<html><h1>James Mbowa is testing</h1></html>'
 end
 
